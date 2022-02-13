bb5893fb75571ea9054daf2ac5e68d5fb64e97ad
Chris Norman <chris.norman2@googlemail.com>
2022-02-13 17:59:49 +0000

    Added the `PlaySoundSemantics` widget.

---
+++ lib/widgets/play_sound_semantics.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:ziggurat/sound.dart';
+import 'package:ziggurat/ziggurat.dart';
+
+/// A widget for playing a sound when selected.
+class PlaySoundSemantics extends StatefulWidget {
+  /// Create an instance.
+  const PlaySoundSemantics({
+    required this.child,
+    required this.soundChannel,
+    this.assetReference,
+    this.gain = 0.7,
+    this.looping = false,
+    Key? key,
+  }) : super(key: key);
+
+  /// The widget below this one in the tree.
+  final Widget child;
+
+  /// The sound channel to play through.
+  final SoundChannel soundChannel;
+
+  /// The sound to play.
+  ///
+  /// If this value is `null`, no sound will play.
+  final AssetReference? assetReference;
+
+  /// The gain for the resulting sound.
+  final double gain;
+
+  /// Whether or not the resulting sound should loop.
+  final bool looping;
+
+  /// Create state for this widget.
+  @override
+  _PlaySoundSemanticsState createState() => _PlaySoundSemanticsState();
+}
+
+/// State for [PlaySoundSemantics].
+class _PlaySoundSemanticsState extends State<PlaySoundSemantics> {
+  PlaySound? _playSound;
+
+  /// Build a widget.
+  @override
+  Widget build(BuildContext context) => Semantics(
+        child: widget.child,
+        onDidGainAccessibilityFocus: () {
+          _playSound?.destroy();
+          final assetReference = widget.assetReference;
+          if (assetReference != null) {
+            _playSound = widget.soundChannel.playSound(
+              assetReference,
+              gain: widget.gain,
+              keepAlive: true,
+              looping: widget.looping,
+            );
+          }
+        },
+        onDidLoseAccessibilityFocus: () {
+          _playSound?.destroy();
+          _playSound = null;
+        },
+      );
+
+  /// Dispose of the play sound instance.
+  @override
+  void dispose() {
+    super.dispose();
+    _playSound?.destroy();
+    _playSound = null;
+  }
+}
