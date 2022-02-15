f3498b1f9e2a70779aec7f199e54113567bd0dc7
Chris Norman <chris.norman2@googlemail.com>
2022-02-15 02:15:13 +0000

    Added the `TextListTile` widget.

---
+++ lib/widgets/text_list_tile.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../util.dart';
+import 'get_text.dart';
+
+/// A list tile that displays and allows the editing of some text.
+class TextListTile extends StatelessWidget {
+  /// Create an instance.
+  const TextListTile({
+    required this.value,
+    required this.onChanged,
+    required this.header,
+    this.title,
+    this.labelText,
+    this.validator,
+    this.autofocus = false,
+    Key? key,
+  }) : super(key: key);
+
+  /// The title of the list tile.
+  final String header;
+
+  /// The current value.
+  final String value;
+
+  /// The function to be called when the value changes.
+  final ValueChanged<String> onChanged;
+
+  /// The title of the [GetText] widget.
+  final String? title;
+
+  /// The label text for the [GetText] widget.
+  final String? labelText;
+
+  /// The validator to use.
+  final FormFieldValidator<String?>? validator;
+
+  /// Whether the resulting [ListTile] should be autofocused.
+  final bool autofocus;
+  @override
+  Widget build(BuildContext context) => ListTile(
+        autofocus: autofocus,
+        title: Text(header),
+        subtitle: Text(value),
+        onTap: () => pushWidget(
+          context: context,
+          builder: (context) => GetText(
+            onDone: (value) {
+              Navigator.pop(context);
+              onChanged(value);
+            },
+            labelText: labelText ?? title ?? header,
+            text: value,
+            title: title ?? header,
+            validator: validator,
+          ),
+        ),
+      );
+}
