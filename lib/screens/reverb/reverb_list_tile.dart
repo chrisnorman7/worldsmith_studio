a9553de18f7a60e0beb28d27f547cc6290fb4065
Chris Norman <chris.norman2@googlemail.com>
2022-02-17 14:39:18 +0000

    Added some widgets for dealing with reverbs.

---
+++ lib/screens/reverb/reverb_list_tile.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:worldsmith/worldsmith.dart';
+
+import '../../project_context.dart';
+import '../../util.dart';
+import 'select_reverb.dart';
+
+/// A list tile for changing a reverb preset.
+class ReverbListTile extends StatelessWidget {
+  /// Create an instance.
+  const ReverbListTile({
+    required this.projectContext,
+    required this.onDone,
+    required this.reverbPresets,
+    this.currentReverbId,
+    this.nullable = false,
+    this.title = 'Reverb Preset',
+    Key? key,
+  }) : super(key: key);
+
+  /// The project context to send to [SelectReverb].
+  final ProjectContext projectContext;
+
+  /// Create an instance.
+  /// The function to be called with the new value.
+  final ValueChanged<ReverbPresetReference?> onDone;
+
+  /// The list of reverb presets.
+  final List<ReverbPresetReference> reverbPresets;
+
+  /// The ID of the current reverb preset.
+  final String? currentReverbId;
+
+  /// Whether or not the new reverb can be `null`.
+  final bool nullable;
+
+  /// The title of the resulting [ListTile].
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    ReverbPresetReference? currentReverbPreset;
+    if (currentReverbId != null) {
+      currentReverbPreset = reverbPresets.firstWhere(
+        (element) => element.id == currentReverbId,
+      );
+    }
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(
+        currentReverbPreset == null
+            ? 'Not Set'
+            : currentReverbPreset.reverbPreset.name,
+      ),
+      onTap: () => pushWidget(
+        context: context,
+        builder: (context) => SelectReverb(
+          projectContext: projectContext,
+          onDone: onDone,
+          reverbPresets: reverbPresets,
+          currentReverbId: currentReverbId,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/reverb/select_reverb.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+import 'package:worldsmith/util.dart';
+import 'package:worldsmith/worldsmith.dart';
+import 'package:ziggurat/sound.dart';
+
+import '../../project_context.dart';
+import '../../widgets/cancel.dart';
+
+/// A widget for selecting from a list of [reverbPresets].
+class SelectReverb extends StatefulWidget {
+  /// Create an instance.
+  const SelectReverb({
+    required this.projectContext,
+    required this.onDone,
+    required this.reverbPresets,
+    this.currentReverbId,
+    this.nullable = false,
+    this.title = 'Select Reverb Preset',
+    this.actions = const [],
+    Key? key,
+  }) : super(key: key);
+
+  /// The project context to use.
+  final ProjectContext projectContext;
+
+  /// The function to be called with the new value.
+  final ValueChanged<ReverbPresetReference?> onDone;
+
+  /// The list of presets to use.
+  final List<ReverbPresetReference> reverbPresets;
+
+  /// The ID of the current preset.
+  final String? currentReverbId;
+
+  /// Whether or not the new preset can be `null`.
+  final bool nullable;
+
+  /// The title of the resulting [AppBar].
+  final String title;
+
+  /// The actions to apply to the resulting [AppBar].
+  final List<Widget> actions;
+
+  @override
+  State<SelectReverb> createState() => _SelectReverbState();
+}
+
+class _SelectReverbState extends State<SelectReverb> {
+  PlaySound? _playSound;
+  SoundChannel? _channel;
+  late final Map<String, CreateReverb> _reverbs;
+
+  @override
+  void initState() {
+    super.initState();
+    _reverbs = {};
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final children = <Widget>[];
+    if (widget.nullable) {
+      children.add(
+        ListTile(
+          autofocus: widget.currentReverbId == null,
+          title: const Text('Clear Reverb'),
+          onTap: () => widget.onDone(null),
+        ),
+      );
+    }
+    for (var i = 0; i < widget.reverbPresets.length; i++) {
+      final reverbPreset = widget.reverbPresets[i];
+      final select = reverbPreset.id == widget.currentReverbId;
+      final sound = reverbPreset.sound;
+      children.add(
+        Semantics(
+          child: ListTile(
+            autofocus: widget.currentReverbId == null ? i == 0 : select,
+            title: Text(reverbPreset.reverbPreset.name),
+            selected: select,
+            onTap: () => widget.onDone(reverbPreset),
+          ),
+          onDidGainAccessibilityFocus: sound == null
+              ? null
+              : () {
+                  var reverb = _reverbs[reverbPreset.id];
+                  if (reverb == null) {
+                    reverb = widget.projectContext.game.createReverb(
+                      reverbPreset.reverbPreset,
+                    );
+                    _reverbs[reverbPreset.id] = reverb;
+                  }
+                  var channel = _channel;
+                  if (channel == null) {
+                    channel = widget.projectContext.game.createSoundChannel(
+                      reverb: reverb,
+                    );
+                    _channel = channel;
+                  } else {
+                    channel.setReverb(reverb);
+                  }
+                  stopSound();
+                  final assetReference = getAssetReferenceReference(
+                    assets: widget.projectContext.world.interfaceSoundsAssets,
+                    id: sound.id,
+                  )!
+                      .reference;
+                  _playSound = channel.playSound(
+                    widget.projectContext.getRelativeAssetReference(
+                      assetReference,
+                    ),
+                    gain: sound.gain,
+                    keepAlive: true,
+                  );
+                },
+          onDidLoseAccessibilityFocus: stopSound,
+        ),
+      );
+    }
+    return Cancel(
+      child: Scaffold(
+        appBar: AppBar(
+          actions: widget.actions,
+          title: Text(widget.title),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) => children[index],
+          itemCount: children.length,
+        ),
+      ),
+    );
+  }
+
+  /// Stop the playing sound.
+  void stopSound() {
+    _playSound?.destroy();
+    _playSound = null;
+  }
+
+  /// Destroy everything.
+  @override
+  void dispose() {
+    super.dispose();
+    stopSound();
+    while (_reverbs.isNotEmpty) {
+      _reverbs.remove(_reverbs.keys.first)!.destroy();
+    }
+    _channel?.destroy();
+    _channel = null;
+  }
+}
