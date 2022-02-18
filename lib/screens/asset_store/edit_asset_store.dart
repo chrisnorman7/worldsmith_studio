2e0b836ee141443491a7d154af4cf12b45b8c948
Chris Norman <chris.norman2@googlemail.com>
2022-02-18 03:04:28 +0000

    Add keyboard shortcuts help to the asset store editor.

--- lib/screens/asset_store/edit_asset_store.dart
+++ lib/screens/asset_store/edit_asset_store.dart
@@ -11,6 +11,7 @@ import '../../project_context.dart';
 import '../../util.dart';
 import '../../widgets/cancel.dart';
 import '../../widgets/center_text.dart';
+import '../../widgets/keyboard_shortcuts_list.dart';
 import '../../widgets/play_sound_semantics.dart';
 import 'add_asset.dart';
 import 'edit_asset_reference.dart';
@@ -65,93 +66,103 @@ class _EditAssetStoreState extends State<EditAssetStore> {
               b.comment.toString().toLowerCase(),
             ),
       );
-    return Cancel(
-      child: Shortcuts(
-        child: Actions(
-          actions: {OpenProjectIntent: addAssetAction},
-          child: Builder(
-            builder: (context) => Scaffold(
-              appBar: AppBar(
-                title: Text('${widget.assetStore.comment}'),
-              ),
-              body: assets.isEmpty
-                  ? const CenterText(text: 'There are no assets in this store.')
-                  : ListView.builder(
-                      itemBuilder: (context, index) {
-                        final assetReference = assets[index];
-                        final relativeAssetReference =
-                            widget.projectContext.getRelativeAssetReference(
-                          assetReference.reference,
-                        );
-                        final String assetSize;
-                        switch (assetReference.reference.type) {
-                          case AssetType.file:
-                            assetSize = filesize(
-                              relativeAssetReference
-                                  .getFile(widget.projectContext.game.random)
-                                  .statSync()
-                                  .size,
-                            );
-                            break;
-                          case AssetType.collection:
-                            final directory = Directory(
-                              relativeAssetReference.name,
-                            );
-                            final fileSizes = [
-                              for (final file
-                                  in directory.listSync().whereType<File>())
-                                file.statSync().size
-                            ];
-                            assetSize = filesize(
-                              fileSizes.reduce(
-                                (value, element) => value + element,
-                              ),
-                            );
-                            break;
-                        }
-                        return PlaySoundSemantics(
-                          child: Builder(
-                            builder: (context) => ListTile(
-                              autofocus: index == 0,
-                              title: Text(assetString(assetReference)),
-                              subtitle: Text(assetSize),
-                              onTap: () async {
-                                PlaySoundSemantics.of(context)!.stop();
-                                await pushWidget(
-                                  context: context,
-                                  builder: (context) => EditAssetReference(
-                                    projectContext: widget.projectContext,
-                                    assetStore: widget.assetStore,
-                                    assetReferenceReference: assetReference,
-                                    canDelete: widget.canDelete,
-                                  ),
-                                );
-                                setState(() {});
-                              },
-                            ),
-                          ),
-                          soundChannel:
-                              widget.projectContext.game.interfaceSounds,
-                          assetReference: relativeAssetReference,
-                          looping: true,
-                        );
-                      },
-                      itemCount: assets.length,
-                    ),
-              floatingActionButton: FloatingActionButton(
-                autofocus: assets.isEmpty,
-                child: const Icon(Icons.add_outlined),
-                onPressed: Actions.handler<OpenProjectIntent>(
-                  context,
-                  _openProjectIntent,
+    return WithKeyboardShortcuts(
+      child: Cancel(
+        child: Shortcuts(
+          child: Actions(
+            actions: {OpenProjectIntent: addAssetAction},
+            child: Builder(
+              builder: (context) => Scaffold(
+                appBar: AppBar(
+                  title: Text('${widget.assetStore.comment}'),
                 ),
-                tooltip: 'Add Asset',
+                body: assets.isEmpty
+                    ? const CenterText(
+                        text: 'There are no assets in this store.')
+                    : ListView.builder(
+                        itemBuilder: (context, index) {
+                          final assetReference = assets[index];
+                          final relativeAssetReference =
+                              widget.projectContext.getRelativeAssetReference(
+                            assetReference.reference,
+                          );
+                          final String assetSize;
+                          switch (assetReference.reference.type) {
+                            case AssetType.file:
+                              assetSize = filesize(
+                                relativeAssetReference
+                                    .getFile(widget.projectContext.game.random)
+                                    .statSync()
+                                    .size,
+                              );
+                              break;
+                            case AssetType.collection:
+                              final directory = Directory(
+                                relativeAssetReference.name,
+                              );
+                              final fileSizes = [
+                                for (final file
+                                    in directory.listSync().whereType<File>())
+                                  file.statSync().size
+                              ];
+                              assetSize = filesize(
+                                fileSizes.reduce(
+                                  (value, element) => value + element,
+                                ),
+                              );
+                              break;
+                          }
+                          return PlaySoundSemantics(
+                            child: Builder(
+                              builder: (context) => ListTile(
+                                autofocus: index == 0,
+                                title: Text(assetString(assetReference)),
+                                subtitle: Text(assetSize),
+                                onTap: () async {
+                                  PlaySoundSemantics.of(context)!.stop();
+                                  await pushWidget(
+                                    context: context,
+                                    builder: (context) => EditAssetReference(
+                                      projectContext: widget.projectContext,
+                                      assetStore: widget.assetStore,
+                                      assetReferenceReference: assetReference,
+                                      canDelete: widget.canDelete,
+                                    ),
+                                  );
+                                  setState(() {});
+                                },
+                              ),
+                            ),
+                            soundChannel:
+                                widget.projectContext.game.interfaceSounds,
+                            assetReference: relativeAssetReference,
+                            looping: true,
+                          );
+                        },
+                        itemCount: assets.length,
+                      ),
+                floatingActionButton: FloatingActionButton(
+                  autofocus: assets.isEmpty,
+                  child: const Icon(Icons.add_outlined),
+                  onPressed: Actions.handler<OpenProjectIntent>(
+                    context,
+                    _openProjectIntent,
+                  ),
+                  tooltip: 'Add Asset',
+                ),
               ),
             ),
           ),
+          shortcuts: const {OpenProjectIntent.hotkey: _openProjectIntent},
         ),
-        shortcuts: const {OpenProjectIntent.hotkey: _openProjectIntent},
       ),
+      keyboardShortcuts: const [
+        KeyboardShortcut(
+          description: 'Add a new asset',
+          keyName: 'o',
+          control: true,
+        )
+      ],
     );
   }
 }
