4fd0ac4d3786343d5d7f7b4912cf9c83ebea42de
Chris Norman <chris.norman2@googlemail.com>
2022-02-19 00:18:02 +0000

    Added widgets for dealing with custom messages.

---
+++ lib/widgets/custom_message/custom_message_list_tile.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:worldsmith/worldsmith.dart';
+import 'package:ziggurat/ziggurat.dart';
+
+import '../../project_context.dart';
+import '../../util.dart';
+import '../play_sound_semantics.dart';
+import 'edit_custom_message.dart';
+
+/// A widget for showing and editing a [customMessage].
+class CustomMessageListTile extends StatefulWidget {
+  /// Create an instance.
+  const CustomMessageListTile({
+    required this.projectContext,
+    required this.customMessage,
+    required this.title,
+    this.assetReference,
+    this.autofocus = false,
+    this.validator,
+    Key? key,
+  }) : super(key: key);
+
+  /// The project context to use.
+  final ProjectContext projectContext;
+
+  /// The message to use.
+  final CustomMessage customMessage;
+
+  /// The title of the resulting [ListTile].
+  final String title;
+
+  /// The sound to use when the [customMessage] doesn't have one.
+  final AssetReference? assetReference;
+
+  /// The autofocus value.
+  final bool autofocus;
+
+  /// The validator to use when editing text.
+  final FormFieldValidator<String>? validator;
+
+  /// Create state for this widget.
+  @override
+  _CustomMessageListTileState createState() => _CustomMessageListTileState();
+}
+
+/// State for [CustomMessageListTile].
+class _CustomMessageListTileState extends State<CustomMessageListTile> {
+  /// Build a widget.
+  @override
+  Widget build(BuildContext context) {
+    final text = widget.customMessage.text;
+    final sound = widget.customMessage.sound;
+    return PlaySoundSemantics(
+      child: Builder(
+        builder: (context) => ListTile(
+          autofocus: widget.autofocus,
+          title: Text(widget.title),
+          subtitle: Text('$text'),
+          onTap: () async {
+            PlaySoundSemantics.of(context)?.stop();
+            await pushWidget(
+              context: context,
+              builder: (context) => EditCustomMessage(
+                projectContext: widget.projectContext,
+                customMessage: widget.customMessage,
+              ),
+            );
+            widget.projectContext.save();
+            setState(() {});
+          },
+        ),
+      ),
+      soundChannel: widget.projectContext.game.interfaceSounds,
+      assetReference: sound == null
+          ? widget.assetReference
+          : widget.projectContext.getRelativeAssetReference(
+              widget.projectContext.worldContext.getCustomSound(sound),
+            ),
+    );
+  }
+}
---
+++ lib/widgets/custom_message/edit_custom_message.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:worldsmith/util.dart';
+import 'package:worldsmith/worldsmith.dart';
+
+import '../../project_context.dart';
+import '../../screens/asset_store/select_asset.dart';
+import '../../screens/asset_store/select_asset_store.dart';
+import '../../screens/sound/gain_list_tile.dart';
+import '../../util.dart';
+import '../cancel.dart';
+import '../play_sound_semantics.dart';
+import '../text_list_tile.dart';
+
+/// A widget for editing a [customMessage].
+class EditCustomMessage extends StatefulWidget {
+  /// Create an instance.
+  const EditCustomMessage({
+    required this.projectContext,
+    required this.customMessage,
+    this.validator,
+    Key? key,
+  }) : super(key: key);
+
+  /// The project context to use.
+  final ProjectContext projectContext;
+
+  /// The custom message to work on.
+  final CustomMessage customMessage;
+
+  /// The validator to use for the text.
+  final FormFieldValidator<String>? validator;
+
+  /// Create state for this widget.
+  @override
+  _EditCustomMessageState createState() => _EditCustomMessageState();
+}
+
+/// State for [EditCustomMessage].
+class _EditCustomMessageState extends State<EditCustomMessage> {
+  /// Build a widget.
+  @override
+  Widget build(BuildContext context) {
+    final text = widget.customMessage.text;
+    final sound = widget.customMessage.sound;
+    final assetStore = sound == null
+        ? null
+        : widget.projectContext.worldContext.getAssetStore(sound.assetStore);
+    final reference = sound == null
+        ? null
+        : getAssetReferenceReference(assets: assetStore!.assets, id: sound.id);
+    return Cancel(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Edit Message'),
+        ),
+        body: ListView(
+          children: [
+            TextListTile(
+              value: text ?? '',
+              onChanged: (value) {
+                widget.customMessage.text = value.isEmpty ? null : value;
+                save();
+              },
+              header: 'Text',
+              autofocus: true,
+              validator: widget.validator,
+            ),
+            PlaySoundSemantics(
+              child: ListTile(
+                title: const Text('Asset'),
+                subtitle: Text(
+                  sound == null
+                      ? 'Not Set'
+                      : '${assetStore!.comment}/${reference!.comment}',
+                ),
+                onTap: () => pushWidget(
+                  context: context,
+                  builder: (context) => SelectAssetStore(
+                    projectContext: widget.projectContext,
+                    onDone: (customSoundAssetStore) {
+                      Navigator.pop(context);
+                      if (customSoundAssetStore == null) {
+                        widget.customMessage.sound = null;
+                        save();
+                      } else {
+                        pushWidget(
+                          context: context,
+                          builder: (context) => SelectAsset(
+                            projectContext: widget.projectContext,
+                            assetStore: widget.projectContext.worldContext
+                                .getAssetStore(customSoundAssetStore),
+                            onDone: (newAssetReference) {
+                              Navigator.pop(context);
+                              if (newAssetReference == null) {
+                                widget.customMessage.sound = null;
+                              } else {
+                                final sound = widget.customMessage.sound;
+                                if (sound == null) {
+                                  widget.customMessage.sound = CustomSound(
+                                    assetStore: customSoundAssetStore,
+                                    id: newAssetReference.variableName,
+                                  );
+                                } else {
+                                  sound
+                                    ..assetStore = customSoundAssetStore
+                                    ..id = newAssetReference.variableName;
+                                }
+                              }
+                              save();
+                            },
+                            currentId: sound?.id,
+                            nullable: true,
+                          ),
+                        );
+                      }
+                    },
+                    currentAssetStore: sound?.assetStore,
+                  ),
+                ),
+              ),
+              soundChannel: widget.projectContext.game.interfaceSounds,
+              assetReference: reference?.reference,
+              gain: sound?.gain ??
+                  widget.projectContext.world.soundOptions.defaultGain,
+            ),
+            if (sound != null)
+              GainListTile(
+                gain: sound.gain,
+                onChange: (value) {
+                  sound.gain = value;
+                  widget.projectContext.save();
+                  setState(() {});
+                },
+              )
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Save the project.
+  void save() {
+    widget.projectContext.save();
+    setState(() {});
+  }
+}
