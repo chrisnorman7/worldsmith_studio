2c2d7ab848bf6f3ddac748ae8b2995bd174919d2
Chris Norman <chris.norman2@googlemail.com>
2022-02-21 16:47:59 +0000

    Added the `SelectItem` class.

---
+++ lib/widgets/select_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+/// A widget for selecting something from a list of [values].
+class SelectItem<T> extends StatelessWidget {
+  /// Create an instance.
+  const SelectItem({
+    required this.onDone,
+    required this.values,
+    this.title = 'Select Item',
+    this.value,
+    this.getDescription,
+    Key? key,
+  }) : super(key: key);
+
+  /// The function to call with the resulting value.
+  final ValueChanged<T> onDone;
+
+  /// The possible values to choose from.
+  final List<T> values;
+
+  /// The function to be used to return a textual representation of values.
+  final String Function(T value)? getDescription;
+
+  /// The title of the resulting [Scaffold].
+  final String title;
+
+  /// The current value.
+  final T? value;
+
+  /// Create the widget.
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final element = values[index];
+          final f = getDescription;
+          final String description;
+          if (f == null) {
+            description = element.toString();
+          } else {
+            description = f(element);
+          }
+          return ListTile(
+            autofocus: (value == null && index == 0) || element == value,
+            selected: element == value,
+            title: Text(description),
+            onTap: () => onDone(element),
+          );
+        },
+        itemCount: values.length,
+      ));
+}
