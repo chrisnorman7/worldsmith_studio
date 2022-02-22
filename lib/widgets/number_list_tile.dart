716407134f84b56fe484479f3827a67c7c1ff16e
Chris Norman <chris.norman2@googlemail.com>
2022-02-22 08:50:57 +0000

    Add the `NumberListTile` widget.

---
+++ lib/widgets/number_list_tile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../util.dart';
+import 'get_number.dart';
+
+/// A list tile that displays and allows the editing of a numerical [value].
+class NumberListTile extends StatelessWidget {
+  /// Create an instance.
+  const NumberListTile({
+    required this.value,
+    required this.onChanged,
+    this.min,
+    this.max,
+    this.title = 'Number',
+    this.autofocus = false,
+    Key? key,
+  }) : super(key: key);
+
+  /// The initial value.
+  final double value;
+
+  /// The function to be called with the new value.
+  final ValueChanged<double> onChanged;
+
+  /// The minimum value.
+  final double? min;
+
+  /// The maximum value.
+  final double? max;
+
+  /// The title for the resulting [ListTile].
+  final String title;
+
+  /// Whether the resulting [ListTile] should be autofocused.
+  final bool autofocus;
+
+  @override
+  Widget build(BuildContext context) => ListTile(
+        autofocus: autofocus,
+        title: Text(title),
+        subtitle: Text(value.toString()),
+        onTap: () => pushWidget(
+          context: context,
+          builder: (context) => GetNumber(
+            value: value,
+            onDone: onChanged,
+            max: max,
+            min: min,
+            title: title,
+          ),
+        ),
+      );
+}
