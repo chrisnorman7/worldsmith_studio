29bec825e675c990fe68a2c1841288badadb73bf
Chris Norman <chris.norman2@googlemail.com>
2022-03-02 10:35:26 +0000

    Added the `roundDouble` method.

--- lib/util.dart
+++ lib/util.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart' as path;
@@ -5,6 +7,14 @@ import 'package:worldsmith/worldsmith.dart';
 import 'package:ziggurat_sounds/ziggurat_sounds.dart';
 
 import 'constants.dart';
+
+/// Round the given [value] to the given number of decimal [places].
+///
+/// This code copied and modified from[here](https://www.bezkoder.com/dart-round-double/#:~:text=Dart%20round%20double%20to%20N%20decimal%20places,-We%20have%202&text=%E2%80%93%20Multiply%20the%20number%20by%2010,12.3412%20*%2010%5E2%20%3D%201234.12).
+double roundDouble(double value, {int places = 2}) {
+  final mod = pow(10.0, places);
+  return (value * mod).round().toDouble() / mod;
+}
 
 /// Push the result of the given [builder] onto the stack.
 Future<void> pushWidget({
