2f73a582d1264b7883156942b6f2cea32faf9061
Chris Norman <chris.norman2@googlemail.com>
2022-03-03 20:53:20 +0000

    Make reverbs searchable.

--- lib/screens/project/project_reverbs.dart
+++ lib/screens/project/project_reverbs.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../project_context.dart';
 import '../../util.dart';
 import '../../widgets/center_text.dart';
+import '../../widgets/searchable_list_view.dart';
 import '../reverb/edit_reverb_preset.dart';
 
 /// A widget for viewing and editing reverb preferences.
@@ -30,24 +31,28 @@ class _ProjectReverbsState extends State<ProjectReverbs> {
     if (reverbPresets.isEmpty) {
       return const CenterText(text: 'There are no reverb presets.');
     }
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        final reverbReference = widget.projectContext.world.reverbs[index];
-        return ListTile(
-          autofocus: index == 0,
-          title: Text(reverbReference.reverbPreset.name),
-          onTap: () async {
-            await pushWidget(
-              context: context,
-              builder: (context) => EditReverbPreset(
-                  projectContext: widget.projectContext,
-                  reverbPresetReference: reverbReference),
-            );
-            setState(() {});
-          },
-        );
-      },
-      itemCount: widget.projectContext.world.reverbs.length,
-    );
+    final children = <SearchableListTile>[];
+    for (var i = 0; i < reverbPresets.length; i++) {
+      final reverbReference = reverbPresets[i];
+      children.add(
+        SearchableListTile(
+          searchString: reverbReference.reverbPreset.name,
+          child: ListTile(
+            autofocus: i == 0,
+            title: Text(reverbReference.reverbPreset.name),
+            onTap: () async {
+              await pushWidget(
+                context: context,
+                builder: (context) => EditReverbPreset(
+                    projectContext: widget.projectContext,
+                    reverbPresetReference: reverbReference),
+              );
+              setState(() {});
+            },
+          ),
+        ),
+      );
+    }
+    return SearchableListView(children: children);
   }
 }
