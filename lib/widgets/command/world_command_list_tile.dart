59a9c0075a82e30fe833117a7413f4bb7da87859
Chris Norman <chris.norman2@googlemail.com>
2022-03-12 08:08:21 +0000

    Allow editing the command from whever a command can be selected.

--- lib/widgets/command/world_command_list_tile.dart
+++ lib/widgets/command/world_command_list_tile.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:worldsmith/worldsmith.dart';
 
+import '../../intents.dart';
 import '../../project_context.dart';
+import '../../screens/world_command/edit_world_command.dart';
 import '../../screens/world_command/select_command_category.dart';
 import '../../screens/world_command/select_world_command.dart';
 import '../../util.dart';
 import '../../world_command_location.dart';
+import '../keyboard_shortcuts_list.dart';
 
 /// A list tile that allows the viewing and editing of a [currentId].
-class WorldCommandListTile extends StatelessWidget {
+class WorldCommandListTile extends StatefulWidget {
   /// Create an instance.
   const WorldCommandListTile({
     required this.projectContext,
@@ -38,54 +41,88 @@ class WorldCommandListTile extends StatelessWidget {
   /// Whether or not the resulting [ListTile] is autofocused.
   final bool autofocus;
 
+  @override
+  State<WorldCommandListTile> createState() => _WorldCommandListTileState();
+}
+
+class _WorldCommandListTileState extends State<WorldCommandListTile> {
   /// Build a widget.
   @override
   Widget build(BuildContext context) {
-    final id = currentId;
+    final id = widget.currentId;
     final WorldCommandLocation? location = id == null
         ? null
         : WorldCommandLocation.find(
-            categories: projectContext.world.commandCategories,
+            categories: widget.projectContext.world.commandCategories,
             commandId: id,
           );
     final currentCategory = location?.category;
     final currentValue = location?.command;
-    return ListTile(
-      autofocus: autofocus,
-      title: Text(title),
-      subtitle: Text(
-        location == null
-            ? 'Not set'
-            : '${location.category.name} -> ${location.command.name}',
-      ),
-      onTap: () => pushWidget(
-        context: context,
-        builder: (context) => SelectCommandCategory(
-          projectContext: projectContext,
-          onDone: (commandCategory) {
-            if (nullable == true && commandCategory == null) {
-              Navigator.pop(context);
-              return onChanged(null);
-            }
-            pushWidget(
-              context: context,
-              builder: (context) => SelectWorldCommand(
-                projectContext: projectContext,
-                category: commandCategory!,
-                onDone: (worldCommand) {
+    return WithKeyboardShortcuts(
+      child: CallbackShortcuts(
+        child: ListTile(
+          autofocus: widget.autofocus,
+          title: Text(widget.title),
+          subtitle: Text(
+            location == null
+                ? 'Not set'
+                : '${location.category.name} -> ${location.command.name}',
+          ),
+          onTap: () => pushWidget(
+            context: context,
+            builder: (context) => SelectCommandCategory(
+              projectContext: widget.projectContext,
+              onDone: (commandCategory) {
+                if (widget.nullable == true && commandCategory == null) {
                   Navigator.pop(context);
-                  Navigator.pop(context);
-                  onChanged(worldCommand);
-                },
-                currentId: currentValue?.id,
-                nullable: nullable,
-              ),
-            );
-          },
-          currentId: currentCategory?.id,
-          nullable: nullable,
+                  return widget.onChanged(null);
+                }
+                pushWidget(
+                  context: context,
+                  builder: (context) => SelectWorldCommand(
+                    projectContext: widget.projectContext,
+                    category: commandCategory!,
+                    onDone: (worldCommand) {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                      widget.onChanged(worldCommand);
+                    },
+                    currentId: currentValue?.id,
+                    nullable: widget.nullable,
+                  ),
+                );
+              },
+              currentId: currentCategory?.id,
+              nullable: widget.nullable,
+            ),
+          ),
         ),
+        bindings: {
+          EditIntent.hotkey: () async {
+            final category = currentCategory;
+            final command = currentValue;
+            if (category != null && command != null) {
+              await pushWidget(
+                context: context,
+                builder: (context) => EditWorldCommand(
+                  projectContext: widget.projectContext,
+                  category: category,
+                  command: command,
+                ),
+              );
+              widget.projectContext.save();
+              setState(() {});
+            }
+          }
+        },
       ),
+      keyboardShortcuts: const [
+        KeyboardShortcut(
+          description: 'Edit the command.',
+          keyName: 'E',
+          control: true,
+        )
+      ],
     );
   }
 }
