103d5cb6fdd9cc2cb68a1b09994dcbc1077991ad
Chris Norman <chris.norman2@googlemail.com>
2022-03-17 17:10:09 +0000

    Release 0.0.3.

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:short_uuids/short_uuids.dart';
 
 /// The application version.
-const appVersion = '0.0.2';
+const appVersion = '0.0.3';
 
 /// The app name.
 const appName = 'Worldsmith Studio';
