cc6ccf5a3a60b1bda9552242f0a7d634150f4557
Chris Norman <chris.norman2@googlemail.com>
2022-03-18 18:57:43 +0000

    Add windows to release filenames.

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,2 +1,2 @@
 @echo off
-flutter build windows --release & copy /y *.dll build\windows\runner\Release & 7z a worldsmith_studio-%1.zip build\windows\runner\Release
+flutter build windows --release & copy /y *.dll build\windows\runner\Release & 7z a worldsmith_studio-windows-%1.zip build\windows\runner\Release
