17f58ddc5ae9b6dd67f30fd95eafa2645067efef
Chris Norman <chris.norman2@googlemail.com>
2022-03-21 09:13:45 +0000

    Include changelog with releases.

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,2 +1,2 @@
 @echo off
-flutter build windows --release & copy /y *.dll build\windows\runner\Release & 7z a worldsmith_studio-windows-%1.zip build\windows\runner\Release
+flutter build windows --release & copy /y *.dll build\windows\runner\Release & copy /y changelog.md build\windows\runner\Release & 7z a worldsmith_studio-windows-%1.zip build\windows\runner\Release
