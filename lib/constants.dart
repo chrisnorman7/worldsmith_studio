9c051e3ddfc79f2089df2d7c08c329055de00fff
Chris Norman <chris.norman2@googlemail.com>
2022-03-21 08:58:57 +0000

    Bumped version.

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:short_uuids/short_uuids.dart';
 
 /// The application version.
-const appVersion = '0.0.7';
+const appVersion = '0.0.8';
 
 /// The app name.
 const appName = 'Worldsmith Studio';
