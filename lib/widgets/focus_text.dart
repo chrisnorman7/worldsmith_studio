d35fa1f63c6c4e1a39153af6f54e4cee26aa5ba9
Chris Norman <chris.norman2@googlemail.com>
2022-04-04 12:24:55 +0100

    Added the `FocusText` widget.

---
+++ lib/widgets/focus_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// Text that can be focused.
+class FocusText extends StatelessWidget {
+  /// Create an instance.
+  const FocusText({
+    required this.text,
+    this.autofocus = false,
+    final Key? key,
+  }) : super(key: key);
+
+  /// The text to show.
+  final String text;
+
+  /// Whether the resulting [Focus] will be autofocused.
+  final bool autofocus;
+
+  /// Build the widget.
+  @override
+  Widget build(final BuildContext context) => Focus(
+        child: Text(text),
+        autofocus: autofocus,
+      );
+}
