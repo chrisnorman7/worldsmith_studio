1d5d49357c6aef4929a247f46724c7b44a41c513
Chris Norman <chris.norman2@googlemail.com>
2022-04-28 12:53:27 +0100

    Start using material 3 (for whatever that's worth).

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         title: appName,
         theme: ThemeData(
           primarySwatch: Colors.blue,
+          useMaterial3: true,
         ),
         home: const HomePage(),
       );
