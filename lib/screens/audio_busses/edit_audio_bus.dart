19b4826419af8e0f7eb467eaaeab0f2786e28ce7
Chris Norman <chris.norman2@googlemail.com>
2022-05-01 11:50:41 +0100

    Allow selecting a reverb preset.

--- lib/screens/audio_busses/edit_audio_bus.dart
+++ lib/screens/audio_busses/edit_audio_bus.dart
@@ -5,6 +5,10 @@ import 'package:worldsmith/worldsmith.dart';
 import '../../project_context.dart';
 import '../../validators.dart';
 import '../../widgets/cancel.dart';
+import '../../widgets/number_list_tile.dart';
+import '../../widgets/push_widget_list_tile.dart';
+import '../../widgets/reverb/reverb_list_tile.dart';
+import '../../widgets/select_item.dart';
 import '../../widgets/sound/gain_list_tile.dart';
 import '../../widgets/text_list_tile.dart';
 
@@ -34,6 +38,39 @@ class EditAudioBusState extends State<EditAudioBus> {
   @override
   Widget build(final BuildContext context) {
     final world = widget.projectContext.world;
+    final panningType = widget.audioBus.panningType;
+    String? xTitle;
+    double? xMin;
+    double? xMax;
+    String? yTitle;
+    double? yMin;
+    double? yMax;
+    String? zTitle;
+    double? zMin;
+    double? zMax;
+    switch (panningType) {
+      case PanningType.direct:
+        // Don't allow editing coordinates.
+        break;
+      case PanningType.angular:
+        xTitle = 'Azimuth';
+        xMin = 0.0;
+        xMax = 360.0;
+        yTitle = 'Elevation';
+        yMin = -90.0;
+        yMax = 90.0;
+        break;
+      case PanningType.scalar:
+        xTitle = 'Balance';
+        xMin = -1.0;
+        xMax = 1.0;
+        break;
+      case PanningType.threeD:
+        xTitle = 'X';
+        yTitle = 'Y';
+        zTitle = 'Z';
+        break;
+    }
     return Cancel(
       child: Scaffold(
         appBar: AppBar(
@@ -70,6 +107,68 @@ class EditAudioBusState extends State<EditAudioBus> {
                   ),
                 )
               ],
+            ),
+            PushWidgetListTile(
+              title: 'Panning Type',
+              builder: (context) => SelectItem<PanningType>(
+                onDone: (value) {
+                  Navigator.pop(context);
+                  widget.audioBus
+                    ..panningType = value
+                    ..x = 0.0
+                    ..y = 0.0
+                    ..z = 0.0;
+                  save();
+                },
+                values: PanningType.values,
+                getItemWidget: (value) => Text(value.name),
+                title: 'Select Panning Type',
+                value: panningType,
+              ),
+              subtitle: panningType.name,
+            ),
+            if (xTitle != null)
+              NumberListTile(
+                value: widget.audioBus.x,
+                onChanged: (value) {
+                  widget.audioBus.x = value;
+                  save();
+                },
+                min: xMin,
+                max: xMax,
+                title: xTitle,
+              ),
+            if (yTitle != null)
+              NumberListTile(
+                value: widget.audioBus.y,
+                onChanged: (value) {
+                  widget.audioBus.y = value;
+                  save();
+                },
+                min: yMin,
+                max: yMax,
+                title: yTitle,
+              ),
+            if (zTitle != null)
+              NumberListTile(
+                value: widget.audioBus.z,
+                onChanged: (value) {
+                  widget.audioBus.z = value;
+                  save();
+                },
+                min: zMin,
+                max: zMax,
+                title: zTitle,
+              ),
+            ReverbListTile(
+              projectContext: widget.projectContext,
+              onDone: (value) {
+                widget.audioBus.reverbId = value?.id;
+                save();
+              },
+              reverbPresets: world.reverbs,
+              currentReverbId: widget.audioBus.reverbId,
+              nullable: true,
             )
           ],
         ),
