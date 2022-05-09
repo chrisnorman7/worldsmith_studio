310152ab4c5da4f0fbd00ff8dc4d9c9d34abd894
Chris Norman <chris.norman2@googlemail.com>
2022-05-09 13:41:39 +0100

    Added the `Clickable` widget.

---
+++ lib/widgets/clickable.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_final_parameters
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// A text widget that can be tapped or otherwise activated with the enter or
+/// space bar keys.
+class Clickable extends StatelessWidget {
+  /// Create an instance.
+  const Clickable({
+    required this.child,
+    required this.onActivate,
+    super.key,
+  });
+
+  /// The child widget to use.
+  final Widget child;
+
+  /// The function to call when this widget is activated.
+  final VoidCallback onActivate;
+
+  /// Build the widget.
+  @override
+  Widget build(final BuildContext context) => CallbackShortcuts(
+        bindings: {
+          const SingleActivator(LogicalKeyboardKey.space): onActivate,
+          const SingleActivator(LogicalKeyboardKey.enter): onActivate,
+        },
+        child: GestureDetector(
+          child: child,
+          onTap: onActivate,
+        ),
+      );
+}
